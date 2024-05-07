d7c11af33916de592bca5cb05acc67835af40fc6
Dean Attias <Dean.Attias@gmail.com>
2024-05-08 01:15:16 +0300

    change localhost to deployed backend url

--- client/src/components/CodeBlockList.tsx
+++ client/src/components/CodeBlockList.tsx
@@ -11,7 +11,7 @@ export default function CodeBlockList() {
   const [codeBlocks, setCodeBlocks] = useState<CodeBlock[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/code-blocks')
+    fetch('https://code-with-tom-deans-projects-729ccfbf.vercel.app/api/code-blocks')
       .then(res => res.json())
       .then(data => setCodeBlocks(data));
   }, []);
